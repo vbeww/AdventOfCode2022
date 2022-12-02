812c20caea01488ce56955d33a35e8d8d96e7065
Vincent.Bergervoet <vbe@woodwing.com>
2022-12-02 10:25:44 +0100

    day2: add alternative solution with simple points lookup table in the comments
    The same would apply for part 1

--- day2/src/RoundNewRule.kt
+++ day2/src/RoundNewRule.kt
@@ -36,6 +36,21 @@ class RoundNewRule(round: String) {
     }
 
     val score = myMove.value + points
+
+    /* Alternative with a score lookup table:
+        val score when(round) {
+            "A X" -> 3
+            "A Y" -> 4
+            "A Z" -> 8
+            "B X" -> 1
+            "B Y" -> 5
+            "B Z" -> 9
+            "C X" -> 2
+            "C Y" -> 6
+            "C Z" -> 7
+            else -> throw IllegalArgumentException("Unknown combination of gestures: $round")
+        }
+    */
 }
 
 private fun RoundNewRule.Move.beats() = when (this)  {
