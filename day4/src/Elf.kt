9e76c32754a70843f3e91b59f56acc16b7a45f9a
Vincent.Bergervoet <vbe@woodwing.com>
2022-12-04 08:14:03 +0100

    day4: Add tests to compare out of range and edges

--- day4/src/Elf.kt
+++ day4/src/Elf.kt
@@ -2,5 +2,4 @@ class Elf(sections: String) {
 
     private val sections = sections.split("-").map { it.toInt() }
     fun containsSections(otherElf: Elf) = sections[0] <= otherElf.sections[0] && sections[1] >= otherElf.sections[1]
-
 }
--- day4/tst/CleanupPairsTest.kt
+++ day4/tst/CleanupPairsTest.kt
@@ -3,8 +3,17 @@ import org.junit.jupiter.api.Test
 
 class CleanupPairsTest {
     @Test
-    fun `an Elf knows if his cleanup sections contain anothers Elfs sections fully`() {
+    fun `an Elf knows if his cleanup sections contain another Elf's sections fully`() {
         assertTrue(Elf("2-4").containsSections(Elf("3-3")))
         assertTrue(Elf("2-8").containsSections(Elf("4-8")))
+        assertTrue(Elf("7-9").containsSections(Elf("9-9")))
+        assertTrue(Elf("7-9").containsSections(Elf("7-7")))
+    }
+
+    @Test
+    fun `an Elf knows if his cleanup sections does not contain part of another Elf's sections`() {
+        assertFalse(Elf("92-123").containsSections(Elf("12-17")))
+        assertFalse(Elf("3-3").containsSections(Elf("2-4")))
+        assertFalse(Elf("4-4").containsSections(Elf("2-4")))
     }
 }
