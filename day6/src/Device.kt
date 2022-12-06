37f598a22f67d8750fcd8cc628cb081d71d7fee0
Vincent.Bergervoet <vbe@woodwing.com>
2022-12-06 08:27:37 +0100

    day 6: Refactor findMarker to expression

--- day6/src/Device.kt
+++ day6/src/Device.kt
@@ -2,9 +2,7 @@ class Device(bufferStream: String) {
     val messageMarkerPosition = findMarker(bufferStream, 14)
     val packetMarkerPosition = findMarker(bufferStream, 4)
 
-    private fun findMarker(bufferStream: String, size: Int): Int {
-        return (size..bufferStream.length).first {
-            bufferStream.substring((it - size) until it).toList().distinct().size == size
-        }
+    private fun findMarker(bufferStream: String, size: Int) = (size..bufferStream.length).first {
+        bufferStream.substring((it - size) until it).toList().distinct().size == size
     }
 }
