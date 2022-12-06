6dde06a32ba7f0f36ca29999fe86981f99e6bec8
Vincent.Bergervoet <vbe@woodwing.com>
2022-12-06 08:15:16 +0100

    day6: Also works for other examples

--- day6/tst/SubRoutineTest.kt
+++ day6/tst/SubRoutineTest.kt
@@ -6,4 +6,13 @@ internal class SubRoutineTest {
     fun `The device can identify the position of the subroutine`() {
         assertEquals(7, Device("mjqjpqmgbljsphdztnvjfqwrcgsmlb").markerPosition)
     }
+
+    @Test
+    fun `Confirm other examples`() {
+        assertEquals(5, Device("bvwbjplbgvbhsrlpgdmjqwftvncz").markerPosition)
+        assertEquals(6, Device("nppdvjthqldpwncqszvftbrmjlhg").markerPosition)
+        assertEquals(10, Device("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").markerPosition)
+        assertEquals(11, Device("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").markerPosition)
+
+    }
 }
