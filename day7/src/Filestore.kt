103a0ed993c850b77083cd23572c04cabd0d0820
Vincent.Bergervoet <vbe@woodwing.com>
2022-12-07 09:02:55 +0100

    day 7: fix recursive function for subfolders

--- day7/src/Filestore.kt
+++ day7/src/Filestore.kt
@@ -2,27 +2,28 @@ class Filestore(input: String) {
     val terminal = input.split("\n")
 
     fun totalOfAtMost100_000(): Long {
-        return recursiveListing(0, "/")
+        return recursiveListing(0, "/").second
                 .map { it.second }
                 .filter { it <= 100_000L }
                 .sum()
 
     }
 
-    private fun recursiveListing(startPointer: Int, folder: String): List<Pair<String, Long>> {
+    private fun recursiveListing(startPointer: Int, folder: String): Pair<Int, List<Pair<String, Long>>>{
         var pointer = startPointer + 1
         var totalFileSize = 0L
         var listOfFolders = mutableListOf<Pair<String, Long>>()
         var directSubFolders = mutableListOf<String>()
         do {
             pointer++
-            if (pointer >= terminal.size) return listOfFolders + (folder to totalFileSize)
+            if (pointer >= terminal.size) return pointer to (listOfFolders + (folder to totalFileSize))
             val line = terminal[pointer]
-            if (line == "\$ cd ..") return listOfFolders + (folder to totalFileSize)
+            if (line == "\$ cd ..") return pointer to (listOfFolders + (folder to totalFileSize))
             if (line.startsWith("\$ cd")) {
-                val subFolders = recursiveListing(startPointer, line.split(" ")[2])
+                val (newPointer, subFolders) = recursiveListing(pointer, line.split(" ")[2])
                 listOfFolders.addAll(subFolders)
                 totalFileSize += subFolders.filter { directSubFolders.contains(it.first)  }.map { it.second }.sum()
+                pointer = newPointer
                 continue
             }
             if (line.startsWith(("dir"))) {
--- day7/tst/NoSpaceLeftTest.kt
+++ day7/tst/NoSpaceLeftTest.kt
@@ -12,4 +12,43 @@ class NoSpaceLeftTest {
         """.trimIndent())
         assertEquals(300, filestore.totalOfAtMost100_000())
     }
+
+    @Test
+    fun `file size of multiple files in single folder`() {
+        val filestore = Filestore("""
+            ${'$'} cd /
+            ${'$'} ls
+            100 b.txt
+            dir a
+            300 b.txt
+        """.trimIndent())
+        assertEquals(400, filestore.totalOfAtMost100_000())
+    }
+
+    @Test
+    fun `folders larger then 100_000 are not counted`() {
+        val filestore = Filestore("""
+            ${'$'} cd /
+            ${'$'} ls
+            100000 b.txt
+            dir a
+            300 b.txt
+        """.trimIndent())
+        assertEquals(0, filestore.totalOfAtMost100_000())
+    }
+
+    @Test
+    fun `also count sub-folder size`() {
+        val filestore = Filestore("""
+            ${'$'} cd /
+            ${'$'} ls
+            100 b.txt
+            dir a
+            300 b.txt
+            ${'$'} cd a
+            ${'$'} ls
+            50 c.txt
+        """.trimIndent())
+        assertEquals(500, filestore.totalOfAtMost100_000())
+    }
 }
