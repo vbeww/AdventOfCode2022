b1b800745b668f0fb6cc1f639d5f0ca50e36a137
Vincent.Bergervoet <vbe@woodwing.com>
2022-12-11 09:41:21 +0100

    day 11: Monkey inspects and throws

--- day11/src/Monkey.kt
+++ day11/src/Monkey.kt
@@ -1,19 +1,28 @@
 class Monkey(monkeyBrain: String) {
-    val startingItems = monkeyBrain.info(0, "Starting items: ")
+    var items = monkeyBrain.info(0, "Starting items: ")
             .split(",").map { it.trim().toInt() }
-    val operation = monkeyBrain.info(1, "Operation: new = old ").split(" ")
+
+    val inspect = monkeyBrain.info(1, "Operation: new = old ").split(" ")
             .let { instruction ->
                 { old: Int ->
                     val rightSide = if (instruction[1] == "old") old else instruction[1].toInt()
                     when (instruction[0]) {
                         "+" -> old + rightSide
                         else -> old * rightSide
-                    }
+                    } / 3
                 }
             }
     val test = monkeyBrain.info(2, "Test: divisible by ").toInt()
     val yesMonkey = monkeyBrain.info(3, "If true: throw to monkey ").toInt()
     val noMonkey = monkeyBrain.info(4, "If false: throw to monkey ").toInt()
+
+    fun inspectItems(): List<Pair<Int, Int>> {
+        val thrown = items.map { worryLevel -> inspect(worryLevel) }.map { worryLevel ->
+            worryLevel to if (worryLevel % test == 0) yesMonkey else noMonkey
+        }
+        items = emptyList()
+        return thrown
+    }
 }
 
 private fun String.info(line: Int, prefix: String) = this.split("\n")[line].trim().removePrefix(prefix)
--- day11/tst/MonkeyTest.kt
+++ day11/tst/MonkeyTest.kt
@@ -11,14 +11,14 @@ class MonkeyTest {
     If true: throw to monkey 2
     If false: throw to monkey 3
 """.trimIndent())
-        assertEquals(listOf(79, 98), monkey.startingItems)
-        assertEquals(38, monkey.operation(2))
+        assertEquals(listOf(79, 98), monkey.items)
+        assertEquals(12, monkey.inspect(2))
         assertEquals(23, monkey.test)
         assertEquals(2, monkey.yesMonkey)
         assertEquals(3, monkey.noMonkey)
     }
 
-
+    @Test
     fun `Test parsing monkey brain with plus`() {
         val monkey = Monkey("""  
   Starting items: 54, 65, 75, 74
@@ -27,10 +27,26 @@ class MonkeyTest {
     If true: throw to monkey 2
     If false: throw to monkey 0
 """.trimIndent())
-        assertEquals(listOf(54, 65, 75, 74), monkey.startingItems)
-        assertEquals(9, monkey.operation(3))
+        assertEquals(listOf(54, 65, 75, 74), monkey.items)
+        assertEquals(3, monkey.inspect(3))
+        assertEquals(5, monkey.inspect(10))
         assertEquals(19, monkey.test)
         assertEquals(2, monkey.yesMonkey)
         assertEquals(0, monkey.noMonkey)
+    }
+
+    @Test
+    fun `Monkey 0 throws 500 and 620 to monkey 3`() {
+        val monkey0 = Monkey("""  
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+""".trimIndent())
+
+        val thrownItems = monkey0.inspectItems()
+
+        assertEquals(listOf(500 to 3, 620 to 3), thrownItems)
     }
 }
